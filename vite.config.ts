86879a348befda23d78c7ad1cc4e1e27d71456a6
LGXprod <mattghanno@gmail.com>
2023-10-14 22:01:28 +1100

    Temp removing wasm from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import wasmPack from 'vite-plugin-wasm-pack';
+// import wasmPack from 'vite-plugin-wasm-pack';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasmPack('./regressor-rs')],
+  plugins: [react()], //, wasmPack('./regressor-rs')
   server: {
     port: 3000,
   },
